43e76efdc41a6a06bf1301af123bc78be5352b8b
Joanadayse <daysejoana3@gmail.com>
2025-10-15 14:54:08 -0300

    ajustes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-
+import * as React from 'react';
 import { CalendarIcon, Graphic, History, List } from '../Icon';
 import { DropdownSelect } from '../DropdownSelect';
 import { NavItem } from '../NavItem';
+
 
 
 
@@ -15,8 +15,8 @@ interface SidebarProps {
 
 
 const options = [
-  { value: 'local1', label: 'Local 1' },
-  { value: 'local2', label: 'Local 2' },
+  { value: 'local1', label: 'São luis' },
+  { value: 'local2', label: 'Caldeiras' },
 ];
 
 export const Sidebar: React.FC<SidebarProps> = ({ className }) => {
@@ -24,25 +24,6 @@ export const Sidebar: React.FC<SidebarProps> = ({ className }) => {
     { value: string; label: string } | null
   >(options[0]);
 
-  // Função utilitária para classes de navegação
-// const getNavLinkClasses = ({ isActive }: { isActive: boolean }) => {
-//   const baseClasses = "flex items-center p-2 rounded-lg font-medium text-lg transition-all duration-200";
-//   const activeClasses = "bg-fuchsia-100 text-fuchsia-500";
-//   const inactiveClasses = "text-gray-600 hover:bg-gray-200";
-
-//   return `${baseClasses} ${isActive ? activeClasses : inactiveClasses}`;
-// };
-
-  // Função utilitária para o filtro de cores dos ícones
-// const getIconFilter = (isActive: boolean) => {
-//   // Filtro para a cor ativa (roxo/fuchsia)
-//   const activeFilter = "invert(22%) sepia(89%) saturate(2601%) hue-rotate(282deg) brightness(92%) contrast(97%)";
-
-//   // Filtro para a cor inativa (cinza)
-//   const inactiveFilter = "invert(17%) sepia(15%) saturate(1325%) hue-rotate(172deg) brightness(94%) contrast(93%)";
-
-//   return isActive ? activeFilter : inactiveFilter;
-// };
 
   return (
    <div className={`w-[250px] flex flex-col justify-start p-6 h-screen bg-white rounded-md border border-gray-200 shadow-2xl ${className}`}>
